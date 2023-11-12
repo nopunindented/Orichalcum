1b1299cc9bf51d9ad65aa2d3ed7fe714b11d3f65
Bach <waridh@ualberta.ca>
2023-11-11 19:44:22 -0700

    Instantiated the project

---
+++ app/src/main/java/com/drone/orichalcum/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.drone.orichalcum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * The main activity of the application. Does not need to be named this way, but the template gave
+ * this to us.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
